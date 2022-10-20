f522235463990574096dccfa118998745ac5f23b
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-20 09:50:03 -0500

    adding code to see the difference of using code with and without dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,25 +18,29 @@ public class Main
             System.out.printf("\"%s\" is a number%n", input);
         }
 
-        String[] arr = input.split(" ");
+        //without using the dependency
 
-        StringBuilder newSentence = new StringBuilder("");
-
-        for(String word : arr)
-        {
-            if(Character.isUpperCase(word.charAt(0)))
-            {
-                word = StringUtils.upperCase(word);
-                word = word.substring(0,1).toLowerCase() + word.substring(1);
-                newSentence.append(word).append(" ");
-
-            }
-            else
-            {
-                newSentence.append(word).append(" ");
-            }
-        }
-        System.out.println(newSentence);
+//        String[] arr = input.split(" ");
+//
+//        StringBuilder newSentence = new StringBuilder("");
+//
+//        for(String word : arr)
+//        {
+//            if(Character.isUpperCase(word.charAt(0)))
+//            {
+//                word = StringUtils.upperCase(word);
+//                word = word.substring(0,1).toLowerCase() + word.substring(1);
+//                newSentence.append(word).append(" ");
+//
+//            }
+//            else
+//            {
+//                newSentence.append(word).append(" ");
+//            }
+//        }
+//        System.out.println(newSentence);
+        //using the dependency
+        System.out.println(StringUtils.swapCase(input));
         System.out.println(StringUtils.reverse(input));
     }
 }
